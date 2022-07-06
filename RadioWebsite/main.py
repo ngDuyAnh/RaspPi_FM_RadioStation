dcdffcd25c543650d620a8ec5453a4405e2bafca
Duy Anh Nguyen <In_Drop@outlook.com>
2022-07-05 19:21:40 -0500

    Update main.py
    Uncomment to try the final round

--- RadioWebsite/main.py
+++ RadioWebsite/main.py
@@ -33,7 +33,6 @@ def main(url):
     cwd = os.getcwd()
     downloadDirectory = r"{}/music".format(cwd)
 
-    '''
     # Get the youtube video object
     video_object = YouTube(url)
 
@@ -43,20 +42,17 @@ def main(url):
     file = video_object.streams.filter(only_audio=True)
     audio.download(filename="file", output_path=downloadDirectory)
     print("Download music in MP4")
-    '''
 
     # file names
     filename = "file"
     newfilename = "Converted" + ".wav"
 
-    '''
     # run the ffmpeg cmd to convert to a specific .wav file
     cmd = "ffmpeg -y -i " + os.path.join(downloadDirectory,
                                       filename) + " -f wav -bitexact -acodec pcm_s16le -ar 22050 -ac 1 " + os.path.join(
         downloadDirectory, newfilename)
     subprocess.run(cmd.split())
     print("Convert.wav")
-    '''
 
     # Turn off the current transition
     if audioProcess is not None:
